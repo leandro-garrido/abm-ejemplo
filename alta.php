30837797cec8f00d455f6bc493b5ee67f02820fb
nazoz-dev <leandrogarrido096@gmail.com>
2020-10-28 18:44:09 -0300

    agrego comentarios adicionales a alta.php

--- alta.php
+++ alta.php
@@ -9,6 +9,8 @@
   $apellido = $_POST['apellido'];
 
   // 3) Preparar la orden SQL
+  // INSERT INTO nombre_tabla (campos_tabla) VALUES (valores_a_ingresar)
+  // Ingresa dentro de la siguiente tabla los siguientes valores
   $consulta = "INSERT INTO alumno (id,dni,nombre,apellido) VALUES ('','$dni','$nombre','$apellido')";
 
   // 4) Ejecutar la orden y ingresamos datos
